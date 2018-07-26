7bcf4f869bd551b17a376e284d4047dc13dc415b
Ben Bariteau <ben.bariteau@gmail.com>
2018-07-26 12:21:51 -0700

    add support for nested addition

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 macro_rules! npn {
     [() -> ()] => {};
     [() -> ($stack:tt)] => {$stack};
+    [($head:tt $($tail:tt)*) -> (+ + $($stack:tt)*)] => { npn!(($($tail)*) -> ($head + + $($stack)*)) };
     [($head:tt $($tail:tt)*) -> ($stack_head:tt + $($stack:tt)*)] => { npn!(($($tail)*) -> (($stack_head + $head) $($stack)*)) };
     [$first:tt $second:tt $($tail:tt)*] => { npn!(($($tail)*) -> ($second $first)) };
 }
@@ -9,6 +10,13 @@ macro_rules! npn {
 mod tests {
     #[test]
     fn basic_add() { assert_eq!(npn!(+ 1 2), 3); }
+    #[test]
+    fn double_add() { assert_eq!(npn!(+ + 1 2 3), 6); }
+    #[test]
+    fn lots_of_add() {
+        // any more than this and we hit the macro recursion limit
+        assert_eq!(npn!(+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32), 528);
+    }
 
     /*
     #[test]
