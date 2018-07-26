ee1c1afac39b80e9bfcbd233194f4d4a88789a26
Ben Bariteau <ben.bariteau@gmail.com>
2018-07-26 13:44:12 -0700

    add multiplication support

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,25 @@ macro_rules! npn {
     [() -> ($stack:tt)] => {$stack};
     [(+ $($tail:tt)*) -> ($($stack:tt)*)] => { npn!(($($tail)*) -> (+ $($stack)*)) };
     [(- $($tail:tt)*) -> ($($stack:tt)*)] => { npn!(($($tail)*) -> (- $($stack)*)) };
+    [(* $($tail:tt)*) -> ($($stack:tt)*)] => { npn!(($($tail)*) -> (* $($stack)*)) };
     [($head:tt $($tail:tt)*) -> (+ $($stack:tt)*)] => { npn!(($($tail)*) -> ($head + $($stack)*)) };
     [($head:tt $($tail:tt)*) -> (- $($stack:tt)*)] => { npn!(($($tail)*) -> ($head - $($stack)*)) };
+    [($head:tt $($tail:tt)*) -> (* $($stack:tt)*)] => { npn!(($($tail)*) -> ($head * $($stack)*)) };
     [($head:tt $($tail:tt)*) -> (+ + $($stack:tt)*)] => { npn!(($($tail)*) -> ($head + + $($stack)*)) };
     [($head:tt $($tail:tt)*) -> (- - $($stack:tt)*)] => { npn!(($($tail)*) -> ($head - - $($stack)*)) };
+    [($head:tt $($tail:tt)*) -> (* * $($stack:tt)*)] => { npn!(($($tail)*) -> ($head * * $($stack)*)) };
     [($head:tt $($tail:tt)*) -> (+ - $($stack:tt)*)] => { npn!(($($tail)*) -> ($head + - $($stack)*)) };
     [($head:tt $($tail:tt)*) -> (- + $($stack:tt)*)] => { npn!(($($tail)*) -> ($head - + $($stack)*)) };
+    [($head:tt $($tail:tt)*) -> (* - $($stack:tt)*)] => { npn!(($($tail)*) -> ($head * - $($stack)*)) };
+    [($head:tt $($tail:tt)*) -> (- * $($stack:tt)*)] => { npn!(($($tail)*) -> ($head - * $($stack)*)) };
+    [($head:tt $($tail:tt)*) -> (+ * $($stack:tt)*)] => { npn!(($($tail)*) -> ($head + * $($stack)*)) };
+    [($head:tt $($tail:tt)*) -> (* + $($stack:tt)*)] => { npn!(($($tail)*) -> ($head * + $($stack)*)) };
     [($head:tt $($tail:tt)*) -> ($stack_head:tt + $($stack:tt)*)] => { npn!(($($tail)*) -> (($stack_head + $head) $($stack)*)) };
     [($head:tt $($tail:tt)*) -> ($stack_head:tt - $($stack:tt)*)] => { npn!(($($tail)*) -> (($stack_head - $head) $($stack)*)) };
+    [($head:tt $($tail:tt)*) -> ($stack_head:tt * $($stack:tt)*)] => { npn!(($($tail)*) -> (($stack_head * $head) $($stack)*)) };
     [($($tail:tt)*) -> ($stack_first:tt $stack_second:tt + $($stack:tt)*)] => { npn!(($($tail)*) -> (($stack_second + $stack_first) $($stack)*)) };
     [($($tail:tt)*) -> ($stack_first:tt $stack_second:tt - $($stack:tt)*)] => { npn!(($($tail)*) -> (($stack_second - $stack_first) $($stack)*)) };
+    [($($tail:tt)*) -> ($stack_first:tt $stack_second:tt * $($stack:tt)*)] => { npn!(($($tail)*) -> (($stack_second * $stack_first) $($stack)*)) };
     [$first:tt $second:tt $($tail:tt)*] => { npn!(($($tail)*) -> ($second $first)) };
 }
 
@@ -43,10 +52,15 @@ mod tests {
     fn adds_and_subtracts() { assert_eq!(npn!(+ - - + 1 2 - 3 + 4 5 6 7), 10); }
 
 
-    /*
     #[test]
     fn basic_multiply() { assert_eq!(npn!(* 3 2), 6); }
+    #[test]
+    fn double_multiply() { assert_eq!(npn!(* 3 * 2 3), 18); }
 
+    #[test]
+    fn adds_substracts_and_multiplies_oh_my() { assert_eq!(npn!(* - - * * + + * * * 2 3 2 3 2 3 2 3 2 3 2), 482); }
+
+    /*
     #[test]
     fn basic_divide() { assert_eq!(npn!(/ 8 4), 2); }
     */
