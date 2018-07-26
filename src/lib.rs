06f0159c1524b5b4b639901392112ac241d1e88a
Ben Bariteau <ben.bariteau@gmail.com>
2018-07-26 12:14:52 -0700

    add basic addition support

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+macro_rules! npn {
+    [() -> ()] => {};
+    [() -> ($stack:tt)] => {$stack};
+    [($head:tt $($tail:tt)*) -> ($stack_head:tt + $($stack:tt)*)] => { npn!(($($tail)*) -> (($stack_head + $head) $($stack)*)) };
+    [$first:tt $second:tt $($tail:tt)*] => { npn!(($($tail)*) -> ($second $first)) };
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    fn basic_add() { assert_eq!(npn!(+ 1 2), 3); }
+
+    /*
+    #[test]
+    fn basic_subtract() { assert_eq!(npn!(- 1 2), -1); }
+
+    #[test]
+    fn basic_multiply() { assert_eq!(npn!(* 3 2), 6); }
+
+    #[test]
+    fn basic_divide() { assert_eq!(npn!(/ 8 4), 2); }
+    */
 }
